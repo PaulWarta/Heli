13ff41e651aa2062c22a5dc80526aac1ffbb8661
Paul Warta <paul@team-warta.de>
2022-06-05 20:18:10 +0200

    Verbesserung in der Kartenanpassung

--- Placeholder/map_picker.ts
+++ Placeholder/map_picker.ts
@@ -2,8 +2,11 @@ function map_picker() {
   const smallMap = document.querySelector('.maps.small') as HTMLElement;
   const largeMap = document.querySelector('.maps.large') as HTMLElement;
   const tester = document.getElementById('MapSizeHelper') as HTMLElement;
+  const textContainer = document.querySelector('main > h1') as HTMLElement;
+  const header = document.querySelector('header') as HTMLElement;
   
   function adjustMap() {
+    let switch_value = calcSwitchValue();
     if (window.innerHeight < switch_value) {
       smallMap.style.display = "none";
       largeMap.style.display = "block";
@@ -11,15 +14,18 @@ function map_picker() {
       smallMap.style.display = "block";
       largeMap.style.display = "none";
     }
+    setTimeout(() => {
+      window.addEventListener('resize', () => {
+        adjustMap();
+      }, { once: true })
+    }, 20);
   }
-
-  let switch_value = parseInt(getComputedStyle(tester).height);
+  
+  function calcSwitchValue(): number {
+    return textContainer.offsetHeight + parseInt(getComputedStyle(textContainer).marginTop) + header.offsetHeight + 0.86 * window.innerWidth;
+  }
   adjustMap();
-
-  window.addEventListener('resize', () => {
-    switch_value = parseInt(getComputedStyle(tester).height);
-    adjustMap();
-  })
+  window.addEventListener('resize', adjustMap, { once: true })
 }
 
 
